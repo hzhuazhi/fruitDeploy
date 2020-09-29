15915c3bb0e2b48fffd440f633927a2bed4d0040
df-HelloWorld <duanfeng_1712@qq.com>
2020-09-29 16:22:51 +0800

    edit_2

---
+++ src/com/xn/manager/controller/merchant/AdminRechargeController.java
@@ -0,0 +1,15 @@
+package com.xn.manager.controller.merchant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 管理员的角色的卡商充值的Controller层
+ * @Author yoko
+ * @Date 2020/9/29 16:21
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("/adminrecharge")
+public class AdminRechargeController {
+}
---
+++ src/com/xn/manager/service/impl/RechargeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.xn.manager.service.impl;
+
+import com.xn.common.dao.BaseDao;
+import com.xn.common.service.impl.BaseServiceImpl;
+import com.xn.manager.dao.RechargeDao;
+import com.xn.manager.model.RechargeModel;
+import com.xn.manager.service.RechargeService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description 卡商充值的Service层的实现层
+ * @Author yoko
+ * @Date 2020/9/28 18:12
+ * @Version 1.0
+ */
+@Service("rechargeService")
+public class RechargeServiceImpl<T> extends BaseServiceImpl<T> implements RechargeService<T> {
+    private static Logger log=Logger.getLogger(IssueServiceImpl.class);
+
+    @Autowired
+    private RechargeDao rechargeDao;
+
+    @Override
+    public BaseDao<T> getDao() {
+        // TODO Auto-generated method stub
+        return rechargeDao;
+    }
+
+    @Override
+    public int updateOrderStatus(RechargeModel model) {
+        return rechargeDao.updateOrderStatus(model);
+    }
+}
